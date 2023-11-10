1524e6525ae910f085b3da49f57a77222d321c6a
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-11-11 08:11:27 +0900

    fix: port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: import.meta.env.PORT,
+        port: import.meta.env.VITE_PORT,
     },
 });
